11802d186f3aad5a6b1867f40115596feebed779
DanP62 <dan.pirvanescu@yahoo.com>
2022-02-10 11:49:58 +0000

    My Commit

---
+++ UtilityKnife/Converters/FarhrenheitToCelsius.cs
@@ -0,0 +1,23 @@
+namespace UtilitiyKnife.Converters
+{
+	public class FahrenheitToCelsius
+	{
+		public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+		{
+			//double _fahrenheithTemp = 0.0;
+			double _celsius = 0.0;
+
+			var step1 = Calculator.Calculator.Subtract(
+				FahrenheitTemp, 32
+			);
+			var step2 = Calculator.Calculator.Multiply(
+				step1, 5.0
+			);
+			_celsius = Calculator.Calculator.Division(
+				step2, 9.0
+				);
+
+			return _celsius;
+		}
+	}
+}
